6c42beb5205e1524ce8ad475e6ab1dd8b4cbb7e0
jeevan0017 <jeevan.p@s.amity.edu>
2025-07-20 20:49:27 +0530

    Added next 5 line of code

--- jsp.py
+++ jsp.py
@@ -4,3 +4,11 @@ if num<=1:
 else:
     is_prime=True
     for i in range(2,int(num**0.5)+1):
+        if num % i==0:
+            is_prime=False
+            break
+
+        if is_prime:
+            print(num,"is aprime number")
+        else:
+            print(num,"is not a prime number")
